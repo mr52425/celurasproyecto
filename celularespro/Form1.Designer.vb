13a2541ecc50c8e0c212a6e0a2b98d5eac868c98
jorgeTY <jorgetapia445@yahoo.com>
2021-11-22 18:23:13 -0600

    se añadió la carpeta imagen

--- celularespro/Form1.Designer.vb
+++ celularespro/Form1.Designer.vb
@@ -25,6 +25,11 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel4 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel5 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel6 = New System.Windows.Forms.LinkLabel()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -43,8 +48,13 @@ Partial Class Form1
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.LightCoral
+        Me.Panel1.Controls.Add(Me.LinkLabel6)
+        Me.Panel1.Controls.Add(Me.LinkLabel5)
+        Me.Panel1.Controls.Add(Me.LinkLabel4)
+        Me.Panel1.Controls.Add(Me.LinkLabel3)
+        Me.Panel1.Controls.Add(Me.LinkLabel2)
         Me.Panel1.Controls.Add(Me.LinkLabel1)
-        Me.Panel1.ForeColor = System.Drawing.Color.Orange
+        Me.Panel1.ForeColor = System.Drawing.Color.LightCoral
         Me.Panel1.Location = New System.Drawing.Point(1, 71)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(1144, 72)
@@ -54,13 +64,79 @@ Partial Class Form1
         '
         Me.LinkLabel1.ActiveLinkColor = System.Drawing.Color.Indigo
         Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LinkLabel1.LinkColor = System.Drawing.Color.AntiqueWhite
-        Me.LinkLabel1.Location = New System.Drawing.Point(11, 23)
+        Me.LinkLabel1.Location = New System.Drawing.Point(22, 23)
         Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(86, 20)
+        Me.LinkLabel1.Size = New System.Drawing.Size(74, 29)
         Me.LinkLabel1.TabIndex = 0
         Me.LinkLabel1.TabStop = True
-        Me.LinkLabel1.Text = "LinkLabel1"
+        Me.LinkLabel1.Text = "Inicio"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.ActiveLinkColor = System.Drawing.Color.Indigo
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel2.LinkColor = System.Drawing.Color.AntiqueWhite
+        Me.LinkLabel2.Location = New System.Drawing.Point(191, 23)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(107, 29)
+        Me.LinkLabel2.TabIndex = 1
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Contacto"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.ActiveLinkColor = System.Drawing.Color.Indigo
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel3.LinkColor = System.Drawing.Color.AntiqueWhite
+        Me.LinkLabel3.Location = New System.Drawing.Point(381, 23)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(195, 29)
+        Me.LinkLabel3.TabIndex = 2
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "¿Quienes somos?"
+        '
+        'LinkLabel4
+        '
+        Me.LinkLabel4.ActiveLinkColor = System.Drawing.Color.Indigo
+        Me.LinkLabel4.AutoSize = True
+        Me.LinkLabel4.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel4.LinkColor = System.Drawing.Color.AntiqueWhite
+        Me.LinkLabel4.Location = New System.Drawing.Point(650, 23)
+        Me.LinkLabel4.Name = "LinkLabel4"
+        Me.LinkLabel4.Size = New System.Drawing.Size(80, 29)
+        Me.LinkLabel4.TabIndex = 3
+        Me.LinkLabel4.TabStop = True
+        Me.LinkLabel4.Text = "Visión"
+        '
+        'LinkLabel5
+        '
+        Me.LinkLabel5.ActiveLinkColor = System.Drawing.Color.Indigo
+        Me.LinkLabel5.AutoSize = True
+        Me.LinkLabel5.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel5.LinkColor = System.Drawing.Color.AntiqueWhite
+        Me.LinkLabel5.Location = New System.Drawing.Point(825, 23)
+        Me.LinkLabel5.Name = "LinkLabel5"
+        Me.LinkLabel5.Size = New System.Drawing.Size(86, 29)
+        Me.LinkLabel5.TabIndex = 4
+        Me.LinkLabel5.TabStop = True
+        Me.LinkLabel5.Text = "Misión"
+        '
+        'LinkLabel6
+        '
+        Me.LinkLabel6.ActiveLinkColor = System.Drawing.Color.Indigo
+        Me.LinkLabel6.AutoSize = True
+        Me.LinkLabel6.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel6.LinkColor = System.Drawing.Color.AntiqueWhite
+        Me.LinkLabel6.Location = New System.Drawing.Point(1022, 23)
+        Me.LinkLabel6.Name = "LinkLabel6"
+        Me.LinkLabel6.Size = New System.Drawing.Size(93, 29)
+        Me.LinkLabel6.TabIndex = 5
+        Me.LinkLabel6.TabStop = True
+        Me.LinkLabel6.Text = "Valores"
         '
         'Form1
         '
@@ -82,4 +158,9 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Panel1 As Panel
     Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel6 As LinkLabel
+    Friend WithEvents LinkLabel5 As LinkLabel
+    Friend WithEvents LinkLabel4 As LinkLabel
+    Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
 End Class
